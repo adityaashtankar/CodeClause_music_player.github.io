6819582cc70ca1aea8e301cc394ac359ff8b8b43
Aditya Ashtankar <adityaashtankar.14@gmail.com>
2023-07-31 09:33:10 +0530

    updated

--- musicplayer.js
+++ musicplayer.js
@@ -1,4 +1,34 @@
+let songs=[
+    {
+        name:'Butter',
+        path:'Songs/song1.mp3',
+        artist:'BTS',
+        cover:'Images/cover1.jpeg',
+        album:"BTS"
+    },
+    {
+        name:'Electric',
+        path:'Songs/song2.mp3',
+        artist:'Katty Perry',
+        cover:'Images/cover2.jpg',
+        album:"Hade Music"
+    },
+    {
+        name:'Fake A Smile',
+        path:'Songs/song3.mp3',
+        artist:'Alan Walker',
+        cover:'Images/cover3.jpg',
+        album:"Endless Summer"
+    },
+    {
+        name:'Incomplete',
+        path:'Songs/song4.mp3',
+        artist:'Jay Sean',
+        cover:'Images/cover4.jpg',
+        album:"Music Video"
+    }
 
+]
 let currentMusic=0;
 const audio=document.querySelector('#song')
 const seekbar=document.querySelector('.rslider')
